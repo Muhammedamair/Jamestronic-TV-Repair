cb01a5b35f1b26f4661be3586ade10784736e385
Muhammedamair <mdamair650@gmail.com>
2026-03-17 07:12:41 +0530

    fix: remove aggressive SW unregistration that caused race condition with PWA autoUpdate

--- src/main.tsx
+++ src/main.tsx
@@ -3,30 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-// Clean up any stale service workers from the PWA plugin
-// This prevents the "stuck loading / need to clear site data" issue
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (const registration of registrations) {
-      registration.unregister().then(success => {
-        if (success) console.log('[SW] Unregistered stale service worker');
-      });
-    }
-  });
-  // Also clear any Workbox caches
-  if ('caches' in window) {
-    caches.keys().then(names => {
-      for (const name of names) {
-        caches.delete(name);
-        console.log('[SW] Cleared cache:', name);
-      }
-    });
-  }
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
